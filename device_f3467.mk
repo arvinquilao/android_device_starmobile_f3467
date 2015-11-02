4ab200fa7f895ae82b4cfe704105b1b61ea90e3a
Arvin Quilao <arquilao@gmail.com>
2015-11-02 08:52:25 +0800

    Move configs folder from root

--- device_f3467.mk
+++ device_f3467.mk
@@ -48,11 +48,11 @@ PRODUCT_PACKAGES += \
 
 # KEYLAYOUT
 PRODUCT_COPY_FILES += \
-    device/starmobile/f3467/rootdir/configs/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl
+    device/starmobile/f3467/configs/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl
 
 # GPS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/configs/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml
+    $(LOCAL_PATH)/configs/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml
 
 # RAMDISK
 PRODUCT_COPY_FILES += \
@@ -67,7 +67,7 @@ PRODUCT_COPY_FILES += \
 
 # PERMISSIONS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/configs/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
+    $(LOCAL_PATH)/configs/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
@@ -93,15 +93,15 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
-    $(LOCAL_PATH)/rootdir/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/rootdir/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/rootdir/configs/audio_policy.conf:system/etc/audio_policy.conf
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # HOSTAPD
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/configs/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
-    $(LOCAL_PATH)/rootdir/configs/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept \
-    $(LOCAL_PATH)/rootdir/configs/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny
+    $(LOCAL_PATH)/configs/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
+    $(LOCAL_PATH)/configs/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/configs/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny
 
 $(call inherit-product, build/target/product/full.mk)
 
